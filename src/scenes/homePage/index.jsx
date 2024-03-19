e9c51a1f6947c63938a6d03de0d77a560318100a
Felipe Brevio <felipebrevio05@gmail.com>
2024-03-19 20:55:15 -0300

    feat coinTicker

--- src/scenes/homePage/index.jsx
+++ src/scenes/homePage/index.jsx
@@ -1,4 +1,4 @@
-import { Alert, Box, Button, Divider, Typography, useMediaQuery } from "@mui/material";
+import { Alert, Box, Button, Divider, Typography, useMediaQuery, useTheme } from "@mui/material";
 import { useSelector } from "react-redux";
 import UserWidget from "scenes/widgets/user/UserWidget";
 
@@ -20,6 +20,7 @@ const HomePage = () => {
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
   const { id, picturePath, emailCheck, clan } = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const theme = useTheme();
     
   const home = () => {
     const top = () => {
@@ -43,10 +44,16 @@ const HomePage = () => {
        </>
       
     }
+    const coinTicker = () => {
+      const url  = `https://coinbrain.com/coins/bnb-0x8c888e9187de82c5aaccd9e9acde6b3d15f4f906/ticker?theme=custom&background=${theme.palette.background.alt.replace('#', '')}&padding=16&type=small&currency=BRL`;
+      return <iframe width="100%" height="104" frameBorder="0" scrolling="no" src={url}></iframe>
+  }
 
     const lastContent = () => {
       return <>
           <MyClanWidget clan={clan} />
+          {isNonMobileScreens ? <><Box m="2rem 0" /></> : <><Divider /></>}
+          {coinTicker()}
           {isNonMobileScreens ? <><Box m="2rem 0" /></> : <><Divider /></>}
           <MetaMaskWidget />
           {isNonMobileScreens ? <><Box m="2rem 0" /></> : <><Divider /></>}
