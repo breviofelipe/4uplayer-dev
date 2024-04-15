7c47376556db235a9783e8f569514590281a917d
Felipe Brevio <felipebrevio05@gmail.com>
2024-04-15 00:12:59 -0300

    feat cofunder

---
+++ src/components/PDFViwer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class PDFViewer extends React.Component {
+
+  render() {
+    
+
+    return (
+      <div>
+        <iframe src='https://res.cloudinary.com/dosghtja7/image/upload/v1713150670/assets/termos/w1kk7kbvms8qdlx12d5h.pdf' width={"100%"} height={"800px"} />
+      </div>
+    );
+  }
+}
+
+export default PDFViewer;
--- src/components/modals/BasicModal.jsx
+++ src/components/modals/BasicModal.jsx
@@ -4,14 +4,18 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 import { useState } from "react";
+import PDFViewer from "components/PDFViwer";
+import FlexBetween from "components/FlexBetween";
 
 const style = {
   position: "absolute",
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "background.paper",
+  width: "50%",
+  display: "flex",
+  flexDirection: "column",
+    bgcolor: "background.paper",
   border: "2px solid #000",
   boxShadow: 24,
   p: 4,
@@ -23,10 +27,10 @@ export default function BasicModal() {
   const handleClose = () => setOpen(false);
 
   return (
-    <div style={{ marginTop: "2rem" }}>
+    <div style={{ marginTop: "2rem"}}>
       <Button onClick={handleOpen}>
       <Typography mt={"0.3rem"} marginRight={"0.5rem"} variant="h3">Seja sócio</Typography>
-                    <img width={"25px"} height={"25px"} src={'https://res.cloudinary.com/dosghtja7/image/upload/v1707940336/assets/w5vviukwefe2hwykn2jt.png'} />
+      <img width={"25px"} height={"25px"} src={'https://res.cloudinary.com/dosghtja7/image/upload/v1707940336/assets/w5vviukwefe2hwykn2jt.png'} />
       </Button>
       <Modal
         open={open}
@@ -34,14 +38,15 @@ export default function BasicModal() {
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
+        
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Termo de aceite
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Texto explicando todas as regras e beneficios....
-          </Typography>
+          
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Termo de aceite
+            </Typography>
+            <PDFViewer />
             <Button>ACEITO</Button>
+          
         </Box>
       </Modal>
     </div>
