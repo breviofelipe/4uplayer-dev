b833d000d6b72e25445e3fa018afabfcc36f175d
Felipe Brevio <felipebrevio05@gmail.com>
2024-07-19 10:11:07 -0300

    feat random user Art

--- src/scenes/loginPage/components/ImagemArtistas.jsx
+++ src/scenes/loginPage/components/ImagemArtistas.jsx
@@ -4,22 +4,58 @@ import './ImagemArtistas.css'; // Importa o arquivo CSS para estilos adicionais
 import { Box, Typography, useTheme } from '@mui/material';
 import FlexBetween from 'components/FlexBetween';
 import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+import { useEffect, useState } from 'react';
 const ImagemArtistas = ({ src, alt, profile }) => {
   const { palette } = useTheme();
+  const [userArt, setUserArt] = useState();
+
+  const randomImage = async () => {
+    let url = process.env.REACT_APP_HOST_LOGIN;
+
+    const response = await fetch(url+"/art", {
+      method: "GET",
+    });
+
+    if(response.ok){
+      let body = await response.json();
+      console.log(body);
+      setUserArt(body);
+    }
+  }
+
+  useEffect(() => {
+    randomImage()
+  },[])
   return (
     <Box display={"flex"} flexDirection={"column"} width={"100%"} bgcolor={"#3700FF"}>
-      <Box className="imagem-container">
+      {!userArt ? <Box className="imagem-container">
         <div style={{
           backgroundImage:`url(${src})`,
         }} alt={alt} className="imagem-responsiva" />
+      </Box> 
+      :
+      <Box className="imagem-container">
+        <div style={{
+          backgroundImage:`url(${userArt.src})`,
+        }} alt={alt} className="imagem-responsiva" />
       </Box>
-      <FlexBetween>
+      }
+      {!userArt ? <FlexBetween>
           <Box display={"flex"} >
             <Typography color={palette.primary.dark} >Artista da semana </Typography>
             <Typography ml={"0.5rem"}>{profile}</Typography>
           </Box>
           <InfoOutlinedIcon />
       </FlexBetween>
+      : <FlexBetween>
+        <Box display={"flex"} >
+          <Typography color={palette.primary.dark} >Artista da semana </Typography>
+          <Typography ml={"0.5rem"}>{userArt.description}</Typography>
+        </Box>
+        <InfoOutlinedIcon />
+    </FlexBetween>
+      
+      }
     </Box>
   );
 };
