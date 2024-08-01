071a9e1e937f46941d873e05e7e7420291f9c9e5
Felipe Brevio <felipebrevio05@gmail.com>
2024-07-31 22:00:19 -0300

    feat twitch

--- src/components/twitch/TwitchEmbed.jsx
+++ src/components/twitch/TwitchEmbed.jsx
@@ -4,7 +4,7 @@ import "./TwitchEmbed.css";
 const TwitchEmbed = ({ embedId }) => {
     return <div className="video-responsive">
     <iframe
-        src={`https://player.twitch.tv/?video=${embedId}&autoplay=false&muted=true&time=0h0m00s&parent=localhost`}
+        src={`https://player.twitch.tv/?video=${embedId}&autoplay=false&muted=true&time=0h0m00s&parent=4uplayer-dev.vercel.app`}
         height="720"
         width="1280"
         allowfullscreen>
