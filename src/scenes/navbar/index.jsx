d8c81fa4f331e8f2712ef65f0a422abb097721b3
Felipe Brevio <felipebrevio05@gmail.com>
2024-08-07 18:58:02 -0300

    feat pagnation

--- src/scenes/navbar/index.jsx
+++ src/scenes/navbar/index.jsx
@@ -91,7 +91,7 @@ const Navbar = () => {
       {/* DESKTOP NAV */}
       {isNonMobileScreens ? (
         <FlexBetween gap="2rem">
-           {user && user.role === 'PLAYER' && <IconButton onClick={() => navigate("/cofunders")} >
+           {user && (user.role === 'PLAYER' || user.role === 'ADMIN') && <IconButton onClick={() => navigate("/cofunders")} >
                 <Tooltip title="Exclusivo Cofundadores">
                   <BookmarkIcon sx={{ color: "gold", fontSize: "25px" }} />
                 </Tooltip>
@@ -174,8 +174,10 @@ const Navbar = () => {
             alignItems="center"
             gap="3rem"
           >
-            { user.role === 'PLAYER' && <IconButton onClick={() => navigate("/cofunders")} >
-                 <BookmarkIcon sx={{ color: "gold", fontSize: "25px" }} />
+            {user && (user.role === 'PLAYER' || user.role === 'ADMIN') && <IconButton onClick={() => navigate("/cofunders")} >
+                <Tooltip title="Exclusivo Cofundadores">
+                  <BookmarkIcon sx={{ color: "gold", fontSize: "25px" }} />
+                </Tooltip>
                 </IconButton> 
             }
             <IconButton
