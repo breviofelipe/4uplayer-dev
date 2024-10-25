e25fd70466d6c18d7c16e56b497897602e92df97
Felipe Brevio <felipebrevio05@gmail.com>
2024-10-25 19:36:23 -0300

    feat ads

--- src/scenes/widgets/ads/AdvertWidget.jsx
+++ src/scenes/widgets/ads/AdvertWidget.jsx
@@ -1,32 +1,12 @@
 import { Typography, useMediaQuery, useTheme } from "@mui/material";
 import FlexBetween from "components/FlexBetween";
 import WidgetWrapper from "components/WidgetWrapper";
-import { useEffect } from "react";
 
 const AdvertWidget = () => {
   const { palette } = useTheme();
   const dark = palette.neutral.dark;
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
-  const medium = palette.neutral.medium;
-
-
-  useEffect(() => {
-    const scriptElement = document.querySelector('script[src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4527229097839562"]')
-    const handleScriptLoad = () => {
-      try{
-        if(window.adsbygoogle){
-          window.adsbygoogle.push({});
-        } else {
-          scriptElement.addEventListener("load", handleScriptLoad);
-          console.log("waiting until adsense lib loaded")
-        }
-      } catch (err) {
-        console.log("error in adsense", err);
-      }
-    }
-    handleScriptLoad();
-},[]);
-  
+  const medium = palette.neutral.medium;  
   return (<WidgetWrapper mobile={!isNonMobileScreens}>
       <FlexBetween>
         <Typography color={dark} variant="h5" fontWeight="500">
